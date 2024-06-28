cb9039d8a886a9beed659c7a91ac214747575955
Maximiliano Romero <maximilianoromerovigo@gmail.com>
2024-06-28 16:33:31 -0300

    Feat: Player and Enemies State Machine Added

---
+++ Character/Enemies/Damageable.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name Damageable
+
+signal on_hit(node: Node, damage_taken : int)
+
+@export var dead_animation_name := String("dead")
+@export var health : float = 20:
+	get:
+		return health
+	set(value):
+		SignalBus.emit_signal("on_health_changed", get_parent(), value - health)
+		health = value
+		
+
+
+func hit(damage : int):
+	health -= damage
+	
+	emit_signal("on_hit", get_parent(), damage)
+
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == dead_animation_name):
+		get_parent().queue_free()
---
+++ Character/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var speed : float = 200.0
+
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine : CharacterStateMachine = $CharacterStateMachine
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction : Vector2 = Vector2.ZERO
+
+func _ready():
+	animation_tree.active = true
+
+func _physics_process(_delta):
+
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+
+	direction = Input.get_vector("left", "right", "jump", "down")
+	
+	if direction.x != 0 && state_machine.check_if_can_move():
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+	update_animation_parameters()
+	update_facing_direction()
+	
+func update_animation_parameters():
+	animation_tree.set("parameters/move/blend_position", direction.x)
+
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
